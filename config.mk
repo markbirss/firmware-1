a67d7e4ee44bd1e64ce121a67ad21f20176f096e
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-06-17 15:30:22 +0200

    update to HEAD of OpenWRT-master (16. Jun 2018)
    14cb05909a ath79: clean up led config script files for all devices
    25ea21d765 ath79: fix WNDR3800 support
    10f8823254 ath79: led-resets: add default-trigger property
    f7a220b2f6 ath79: fix usb led on wr-1043nd-v1
    ab5b359876 ath79: fixup TL-WR703N/TL-MR10U split
    3e0489dcd9 tools: build squashfs (v3) for ath79
    8956eb502d ramips: add support for TP-Link TL-WR842N v5
    82fa7b9d78 ramips: use phytpt trigger for mt76 wireless
    291e34edff ramips: fix mt7688 watchdog register base addr
    5da2c68d00 ramips: mt7621: fix mtu setting with kernel 4.14
    2f3c5fe962 mt76: update to the latest version
    70f4845777 ltq-vdsl-fw: strip legacy dsl_fw logic
    c8ac28237d ltq-vdsl-app: use downloaded vectoring firmware
    0653e31270 lantiq-vdsl-fw: update to provide recent vectoring firmware
    8e2f8b0c83 Revert "lantiq: get rid of ltq-vdsl-fw"
    f47d7df6a8 ucert: fix build
    5e9470a93b libjson-c: fix host-build
    8428156f48 package/libs/libnfnetlink: Remove dead mirror
    56e3a19ad6 libubox: make sure blobmsg-json is included in host-build
    6fc8e06078 libjson-c: add host build (for libblobmsg-json)
    1b8f3d9c2e ucert: add package
    037ef13a16 brcm2708: move wifi calibration EEPROMs out of base-files
    a2f17214cd brcm2708: add brcmfmac-firmware-43455-sdio to DEFAULT_PACKAGES
    3a40a7d5a5 brcm2708: add RPi3+ wifi calibration data
    2826471254 linux-firmware: add firmware for BCM43455 SDIO wlan found on RPi3B+
    5101515375 kernel: fix conntrack fixup of offloaded flows on timeout
    68ab89854f kernel: fix conntrack leak for flow_offload connections
    e820734f79 kernel: allow hardware NAT offload drivers to keep a priv pointer
    ec502cd3fe ramips: rename ethernet driver folder to the same one that upstream uses
    b560c1748a kernel: avoid flow offload for connections with xfrm on the dst entry (should fix IPSec)
    df02e7a3c7 tools/bison: Update to 3.0.5
    79bab45772 popt: Add backup site
    651a62353b tools/e2fsprogs: Update to 1.44.2
    3b6f865b01 base-files: sysupgrade: fix handing get_image unpack commands
    c22117a791 toolchain/glibc: update to latest 2.26 commit
    31f35be016 odhcp6c: user string option support
    8d60f6ee40 dnsmasq: fix confdir option processing (FS#1572)

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=83483ba787e5c972099da1e983e95f396961c7f2
+OPENWRT_COMMIT=14cb05909a8dc6e42008697b4fe47b8fa00789d8
 SET_BUILDBOT=env
 MAKE_ARGS=
