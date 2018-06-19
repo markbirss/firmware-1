7552f74af69402a16cc818429dc95a3bed28069c
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-06-19 08:31:42 +0200

    update to HEAD of OpenWRT-master (18. Jun 2018)
    ccc728a0e2 busybox: update to 1.28.4
    83f31b7947 ltq_atm: burn ifx_atm_alloc_tx with fire
    591780615b build: add busybox support to `time` prereq-check
    b1a173d7c3 ath79: add support for tl-wr1043nd v2/v3
    0e81968ee8 ath79: add initial nand support for gl-ar300m
    fe3b62bbdc ath79: initial gl-ar300m support
    142477e751 b43-tools: update to latest git HEAD
    414ce80c07 mvebu: enable CONFIG_HW_RANDOM
    163ab9135a kernel/modules: add chaoskey module, hardware TRNG
    dcfe2a461e include/image-commands.mk: shorter version in Netgear factory header
    ee1dbffeed ipq806x: D7800 only has a single sata port
    076d2ea682 mips: fix dynamic ftrace
    5297a759ae mbedtls: Cosmetic cleanups
    e820455198 mvebu: backport ahci_mvebu errata patchset
    ff0f3522b7 uboot-oxnas: fix typo accidentally committed during oxnas reboot
    c8e76cd4ff ath79: fix rx ring buffer stall qca956x
    cbf69fb2ad cron: add procd listeners for crontabs
    bb71a3f27e ar71xx: add kmod-usb-ehci to fix USB on RB hAP AC
    1ee98fdef3 nftables: bump to version 0.9.0
    9d6a0352e7 libnftnl: bump to version 1.1.1
    4b280ad91a ipq40xx: add support for ZyXEL WRE6606
    71ae0792ba ath79: add tl-wr1043nd-v4 support everything is working
    cad843e1f5 ath79: add ubiquiti unifi ap ac lite/lr
    52109ce71f ath79: ag71xx: fix pll-data setting for ar7242/ar934x/qca955x/qca956x
    97c5cbc496 ath79: add support for Phicomm K2T
    8f804f42d5 ipq40xx: essedma: fixup ip align
    c84ef1f188 iptables: increment PKG_RELEASE to force update
    d600de3ddd kernel: atm: pppoatm fix vc-mux connection failures
    0276e1f760 lantiq: atm: fix ifx_atm driver integration
    510f2efab6 kernel: bump 4.14 to 4.14.50
    db73c71e8d kernel: bump 4.9 to 4.9.109
    93cc44c433 bcm53xx: exit if writing kernel during sysupgrade fails
    7271eacb72 mt76: update to the latest version
    c3d9109fad bcm53xx: drop not ready support for Luxul devices
    c446e38c86 mac80211: backport brcmfmac changes from kernel 4.18
    7e8eb7f309 mac80211: backport brcmfmac firmware & clm_blob loading rework
    f5bce268b0 mvebu: replace espressobin spi patch with upstream one
    772258044b mvebu: reduce speed to gen1 for espressobin pcie
    4ec8c8c23e mvebu: add fix for armada 37xx cpufreq driver
    9a26a9e8b9 uboot-sunxi: update Orange Pi R1 and Zero Plus
    f2135e7811 uboot-sunxi: update to version 2018.05
    7afe1caf70 arm-trusted-firmware-sunxi: update to version from 2018-02-10
    6745af9a0d mediatek: mt7622: Do not deactivate CONFIG_BLK_DEV
    10ce015c65 mediatek: mt7622: Do not set CPU_SUBTYPE for Cortex A53
    1e0aeb2b55 ath79: move Netgear build code into a common makefile
    2b9885571b kernel: bump 4.14 to 4.14.49
    8c1f088521 kernel: bump 4.9 to 4.9.108
    4fc005197a ipq40xx: add get_status_led to diag.sh
    2314c2c6f2 base-files: fix links in banner.failsafe
    5b2e20807d base-files: exit if mtd write command fails during sysupgrade
    1c1e254244 gemini: Cook SQ201 images
    d8072a3530 gemini: Add appropriate Wireless kernel modules
    dc7f540ce1 gemini: Cook a WRGG firmware image
    3c8bb92655 mac80211: backport brcmfmac data structure rework
    0da9303e5b mac80211: backport "brcmfmac: cleanup and some rework" from 4.17
    e53d0da775 bcm53xx: fix NAND partitions on D-Link DIR-885L

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=14cb05909a8dc6e42008697b4fe47b8fa00789d8
+OPENWRT_COMMIT=ccc728a0e2a7c9717d3d1bcc59f0462e9dbed01e
 SET_BUILDBOT=env
 MAKE_ARGS=
