32a97006f6697bafec4aede89a1b5509a8175619
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-06-19 22:05:31 +0200

    update to HEAD of OpenWRT-master (19 Jun 2018)
    * fix ath79
    
    189815462c ramips: add support for ELECOM WRC-1167GHBK2-S
    4645a6d318 ipq806x: add support for NEC Aterm WG2600HP
    4bb6ee465e ramips: mt7621: fix wireless package selection
    512c57e7f3 ltq-vdsl-fw: add nonshared flag
    56f3aee531 arc770: bump kernel to 4.14
    0845a34666 mwlwifi: update to version 10.3.8.0-20180615
    81d446b045 ar71x: add support for Mikrotik Routerboard wAP R
    9a4253b81f ramips: improve ethernet driver performance with GRO/TSO
    c49ae68cba ath79: fix qm5p-ac-v2 dts file
    b4bf43c667 ar71xx: add support for Fritz!WLAN Repeater 450E
    abb4ab076f ar71xx: fix incorrect speed setting on QCA9556
    d661a5d754 kernel/modules: kmod-chaoskey: Add missing dependency

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=ccc728a0e2a7c9717d3d1bcc59f0462e9dbed01e
+OPENWRT_COMMIT=189815462cda469f6bba2609d65a4591f83418e1
 SET_BUILDBOT=env
 MAKE_ARGS=
