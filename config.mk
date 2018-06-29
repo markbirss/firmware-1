09c07bc37ef76ce7b567e639b1265cdc29a9966d
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-06-29 06:04:21 +0200

    reduce package-list to build with vanilla openwrt

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
-TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
+TARGET=ar71xx-mikrotik
+PACKAGES_LIST_DEFAULT=default
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=189815462cda469f6bba2609d65a4591f83418e1
 SET_BUILDBOT=env
