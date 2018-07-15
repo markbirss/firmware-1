f3597c3b024df4be93e72586f66fe57708f0d790
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-07-15 09:13:41 +0200

    update to HEAD of OpenWRT-master (14 Jul 2018)
    01df4a2565, 0c285bd081, 2601e34fad might bring improvements for https://github.com/freifunk-berlin/firmware/issues/494
    
    b123921a92 include/prereq-build.mk: explicitly check for -f flag when using busybox time
    36fa1bbf6f include/kernel-build.mk: fix kernel rebuild on backport patch changes
    18533ff415 kernel: backport page fragment API changes from 4.10+ to 4.9
    888a15ff83 ppp: add missing -fPIC to rp-pppoe.so CFLAGS
    2601e34fad ramips: ethernet: disable fraglist support
    154c0c4006 ubus: compile with LTO enabled
    73fc67b614 procd: compile with LTO enabled
    47b42137ce dropbear: compile with LTO enabled
    ef96d1e34a firewall: compile with LTO enabled
    ef16a394d2 iw: compile with LTO enabled
    e7397eef69 ppp: compile with LTO enabled
    dfbd49bd22 ppp: fix linker flags for the radius plugin
    07940acc34 netifd: compile with LTO enabled
    8c11133c9d busybox: compile with LTO enabled
    4e56af5ab4 mt76: update to the latest version
    16035a7dd3 include/feeds.mk: rework generation of opkg distfeeds.conf
    6dac434c00 base-files: fix feed list in PKG_CONFIG_DEPENDS
    9af22f1ac9 include/feeds.mk: always add available feeds to PACKAGE_SUBDIRS
    6bdd5d8459 scripts/feeds: add src-dummy method
    0c285bd081 ramips: ethernet: use own page_frag_cache
    01df4a2565 ramips: ethernet: use skb_free_frag to free fragments
    2eeb4b78c6 ramips: TP-Link TL-WR902AC v3: add missing wps button
    33321ebefa ramips: TP-Link TL-WR902AC v3: don't build factory image
    a07e1126bc tools: kernel2minor: update to latest version
    11d6547455 config: extend small_flash feature
    cf7154db07 kernel: only optimized for size if small_flash
    621fa91a82 ar71xx: move boards to tiny subtarget
    671999157d verbose.mk: quote SUBMAKE options
    12915b105a arc: Update variables substitutions in u-boot env files
    d238c7f995 mediatek: Fix memory node for U7623
    d3b8e6b2a7 kernel: gpio-nct5104d remove boardname check
    af70d86d62 netifd: update to latest git HEAD
    33553a11ab ramips: clean up and fix MT7621 NAND driver issues
    21ee8ce9b5 kernel: replace bridge port isolate hack with upstream patch backport on 4.14
    68f9921ed8 netifd: update to the latest version
    41a1c1af4b kernel: adjust bridge port isolate patch to match upstream attribute naming
    e07ad61aec procd: update to the latest version, fixes gcc 8 build error
    8b42a260ed mac80211: Expose support for ath9k Dynack
    ba2b0f0ac6 kernel: bump 4.14 to 4.14.54
    954faac7bc qos-scripts: fix indentation
    4630159294 wireguard: bump to 0.0.20180708
    7e82418372 iproute2: update to 4.17.0
    6dac92a42e hostapd: build with LTO enabled (using jobserver for parallel build)
    9b965d3b71 binutils: remove version 2.27
    7c3e3eb098 binutils: update to version 2.30, resolves issues with LTO
    55055aee50 binutils: backport an upstream fix for a linker bug that triggers with LTO
    7ddba08d87 kernel: bcm47xxpart: fix getting user-space data partition name
    a5188eb258 nasm: disable LTO, remove host specific workarounds
    98a6bee09a odhcpd: update to latest git HEAD
    e204717ef2 toolchain/nasm: force ar and ranlib only on macOSX
    79b38047b9 build: README punctuation pendantry
    5781fc6b3f build: Update README & github help
    edf338f248 basefiles: Reword sysupgrade message
    6476148034 ath79: add support for OCEDO Raccoon
    da6c09eff4 kernel: move CONFIG_USB_MTU3 to generic config
    29fa9ac559 kernel: disable some DRM_PANEL config options

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=328ddcf628119733c511ec6cda06f2f0cc8d19ae
+OPENWRT_COMMIT=b123921a92a957f08abb186e041aa38aa9328f3e
 SET_BUILDBOT=env
 MAKE_ARGS=
