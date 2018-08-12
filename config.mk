647668e74f70a11a3b58498f8a568ee81c729a1e
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-08-12 13:10:37 +0200

    verbose build to debug ar71xx-mikrotik intermittent fails

--- config.mk
+++ config.mk
@@ -5,4 +5,4 @@ PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=8e4e545ecbbeb4ef45f07267e496aab66fcbbee3
 SET_BUILDBOT=env
-MAKE_ARGS=
+MAKE_ARGS=V=s
