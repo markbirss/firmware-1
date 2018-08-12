4bd51696c6a3088bc1397f41110c5fb0cefb26e2
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-08-12 13:06:29 +0200

    update OpenWrt to HEAD of 18.06-branch (11. Aug 2018)
    8e4e545ecb ath79: fix eth pll for ar913x
    a6369206fe ath79: add support for I-O DATA WN-AC1600DGR2
    97de133368 ath79: add support of D-Link DIR-825 B1
    a441c86d93 ath79: add ath9k calibration data MAC addresses patching
    dfee452713 base-files: add function to get mac as text from flash
    ec28d2797c base-files: use consistent coding style
    d4e3a31f6c ath79: add support for TP-Link TL-WDR4900 v2
    3493c1cf41 uci: bump to source date 2018-08-11
    0ddb34b6b5 kernel: bump 4.14 to 4.14.62
    21a229317f kernel: bump 4.9 to 4.9.119
    1961948585 wpa_supplicant: fix CVE-2018-14526
    caebb77b45 tools: findutils: fix compilation with glibc 2.28
    8ae8489986 tools: m4: fix compilation with glibc 2.28
    78b5764fd8 base-files: make wifi report unknown command
    c4931713df ath79: add support for OCEDO Koala
    387736af41 ath79: ag71xx: remove PHY reset
    23519edbca ath79: add support for Buffalo BHR-4GRV2
    2e02fdb363 odhcp6c: apply IPv6/ND configuration earlier
    6555612783 ath79: fix PLL settings for QCA955x
    995a1ba841 ath79: add pll for archer c7
    e0fbf62821 iproute2: remove libutil from InstallDev section
    6579af7a77 netifd: update to latest git HEAD
    5f4502998a sunxi: change Orange Pi PC2 MUSB/power button
    e2e39328d5 sunxi: Added support for Xunlong Orange Pi PC2
    14b6c72541 kernel: move e1000e patches to backports
    f4d3047671 firmware: intel-microcode: bump to 20180703
    8a3582fa8a busybox: prevent compile hang with bzip2 enabled
    cc21dab6cc ucert: update to lastest git HEAD
    915b0ba694 cns3xxx: correct size specifier in watchdog init print
    85a0bca7cf cns3xxx: delete invalid snip in patch
    4e39e213af ar71xx: allow to override at803x sgmii aneg status
    f4f99ec973 ar71xx: fix QCA955X SGMII link loss
    fbe17867da kernel: allow device-tree configuration of at803x
    fefe1da440 kernel: backport upstream fix for CVE-2018-5390
    4d961538f6 iproute2: add libutil to InstallDev section
    0ef25a7aee kernel: remove linux 4.4 support
    4b9680f138 ath79: fix QCA9557 eth PLL settings
    5107ba2374 uboot-envtools: add ath79 target
    ddf64edfe1 mediatek: Fix amount of memory on U7623
    b94b4b0862 ath79: don't include tl-wdr3600 image build code for tl-wdr4300
    12121e643b ath79: cleanup netgear wnr612-v2 supported devices
    38877d8873 ath79: mark netgear variables as device specific
    1edba538a9 ath79: Fix led nodes for TL-WR740N v2 and add its clones
    55ff2951ea ath79: fix dts warnings
    bf078fcab6 ath79: fix node names
    a8ad7b6efa ath79: fix compatible strings
    7b42247182 ath79: fix whitespace issue in dts files
    3b1213a377 ramips: use #include syntax for dtsi files
    8174853c78 base-files: introduce sysupgrade signature chain verification
    ec78f03de5 image: fix build without ucert
    848b455d2e image: use ucert to append signature
    7a52ce3faf ucert: update source
    73100024d3 libubox: set HOST_BUILD_PREFIX
    a5368dc30c libjson-c: set HOST_BUILD_PREFIX
    1c4a255aa1 libubox: fix source version date
    432eaa940f libubox: fix mirror hash
    7402cc7160 toolchain/glibc: update to latest 2.26 commit
    33fd1d0d91 ustream-ssl: update to latest git HEAD
    0d283c0180 cns3xxx: ethernet: use circular queue checks consistently
    06beefd6d5 cns3xxx: ethernet: cleanup code
    23cdbf2644 cns3xxx: ethernet: fix signed/unsigned comparison
    d1aa6bd698 cns3xxx: add myself as maintainer
    7a254aeeb8 kernel: bump 4.14 to 4.14.61
    f7036a34ac kernel: bump 4.9 to 4.9.118

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9537c1a153591d2f6082a797d1d088c5d921e9cc
+OPENWRT_COMMIT=8e4e545ecbbeb4ef45f07267e496aab66fcbbee3
 SET_BUILDBOT=env
 MAKE_ARGS=
