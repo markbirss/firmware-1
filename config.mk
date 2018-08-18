c5b0477f2fc925b1cc8ef7a2b1a98c3c9292b79d
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-08-19 00:33:27 +0200

    update to HEAD of OpenWRT-master (18. Aug 2018)
    d1ea8ac3b4 util-linux: Update to 2.32.1
    ff4bc483fc ath79: add support for I-O DATA WN-AC1167DGR
    94ee5e3d1d ath79: fix leds and network for TP-Link TL-WR841 v9/v11
    3f09adaa55 ath79: don't use the pcs,cr5000 wps led as usb led
    0c5a532553 ath79: add support for TP-Link MR-3040 v2
    9e6f22e309 ath79: add support for Buffalo WHR-G301N
    c9e9a78734 lantiq: add support for upgrade led
    be06c726c4 treewide: drop unused get_status_led functions
    dd448cd276 treewide: fix upgrade led handling
    56b8ac1e86 treewide: consolidate upgrade state set
    1ea1f3a223 ramips: mt7620: fix bad indent
    22ae14d0e7 cns3xxx: fix mtu setting with kernel 4.14
    8c91807214 rpcd: update to latest git HEAD
    d74d6c4522 openssl: update to version 1.0.2p
    b547ab3143 kernel: bump kernel 4.9 to version 4.9.120
    e4bad5f0ac kernel: bump kernel 4.14 to version 4.14.63
    ae9c3105a3 ath79: use both WNDR3x00 power leds for boot status indication
    3d9610aa13 ath79: use both DIR-825 B1 power leds for boot status indication
    7a873974f7 ath79: add support for indicating the boot state using multiple leds
    37af596593 ramips: add support for D-Link DWR-118-A2
    4877ad44c7 ramips: mt7620: enable all ports unconditionally
    5762efd8b2 libubox: set RPATH for host build
    e2791e80cb netifd: update to latest git HEAD
    d20f4fc628 ramips: add support for HiWiFi HC5861B
    1213504750 ath79: add support for TP-Link TL-WR941N/ND v2/v3
    8db6522531 ath79: add support for TP-Link WR841N/ND v11
    18f738d3c4 ath79: move TP-Link WR841v9 aliases node from dtsi to dts
    cc35c91f56 ath79: fix sysupgrade TP-Link WR841v9
    9c6c7d11c7 firmware-utils/mksercommfw: fix build with clang/macOS
    437e35f8a3 ath79: Add wifi to WNDR3700, WNDR3700v2 and WNDR3800
    b2a33d5463 ath79: add support for Pisen WMM003N (Cloud Easy Power)
    03e5dcbf10 firewall: bump to git HEAD
    ee9e0c1deb ar7: remove linux 3.18 support
    22b8237ba1 firmware-utils/mksercommfw: fix musl build
    d6844b3bfa ramips: add missing USB packages into ASL26555-16M
    0578993a41 tools/cmake: Update to 3.12.1
    3337f202d6 ath79: drop tl prefix for TP-Link RE450 v2
    f3ffac90bc ath79: gmac: add parsers for rxd(v)- and tx(d|en)-delay for AR9344
    7b1b49bd32 packages: nvram: make it possible to include it for ath79 targets
    09ac4aa86b uboot-zynq: update to 2018.07
    7d1b742b4d ath79: add QCA956x GMAC config
    2276bc0082 brcm47xx: cosmetic fix in model detection
    cf50f72069 ath79: ar913x: fix eth pll register
    42b3fdf981 ath79: ag71xx: fix speed applied to MII0/1_CTRL on ar71xx/ar913x
    4dc7cb792a ath79: add WNDR3700 and WNDR3700v2
    b58df9919e ath79: create WNDR3700 series .dtsi and adjust WNDR3800
    1d3ea7874d ramips: fix BR-6478ACv2 support
    5543d63fc8 ramips: add support for Netgear R6120
    e5b802b9c2 firmware-utils: add sercomm/netgear tool
    42dc0e2594 wireguard: bump to 0.0.20180809
    31aeae0774 at91: do not build image for at91-q5xr5

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=8e4e545ecbbeb4ef45f07267e496aab66fcbbee3
+OPENWRT_COMMIT=d1ea8ac3b476e1e552f42dbf1042b521c57a0df4
 SET_BUILDBOT=env
 MAKE_ARGS=-j1 V=s
