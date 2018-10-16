94f8acdc4933c99595945ee53ecdfcd17df8a2c2
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-10-16 11:33:18 +0200

    Revert "debug: build non-parallel only"
    This reverts commit 95e089b849770e2dd48db878d186036909479392.

--- config.mk
+++ config.mk
@@ -5,4 +5,4 @@ PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=0308385d1d78b81ae8fb05cb588fb91f646e3c37
 SET_BUILDBOT=env
-MAKE_ARGS=-j1 V=s
+MAKE_ARGS=V=s
